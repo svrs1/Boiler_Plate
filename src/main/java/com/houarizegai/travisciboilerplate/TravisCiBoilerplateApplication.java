992358e762be70af9810ec476f7d82b0b5497331
svrs1 <svrs.scm@gmail.com>
2021-03-20 17:09:10 +0530

    adding all the project files to remote

---
+++ src/main/java/com/houarizegai/travisciboilerplate/TravisCiBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.travisciboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiBoilerplateApplication.class, args);
+	}
+
+}
